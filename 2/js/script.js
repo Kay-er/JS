3ae1fadde511c7385af97707b126c1e5bd3b573f
Kay-er <72309798+Kay-er@users.noreply.github.com>
2020-11-19 15:21:10 +0300

    Выполнил задание №2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,5 +1,14 @@
+// Задание № 1
 var name = prompt('Введитет имя:')
 alert('Здравствуйте,'+ name);
 var Tc = prompt('Введите число:');
 Tf = Tc * 1.8 + 32;
 alert(Tf);
+
+// Заадние № 2
+var admin;
+var name = "Василий";
+admin=name;
+console.log(admin);
+
+// Задание № 4 Ответ: 1000108
